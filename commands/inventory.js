95818f74b12cd7127241d74c3f15ef976adddd7d
Shawn Biddle <shawncplus@gmail.com>
2017-02-25 21:07:28 -0500

    Fixes #150 Rename core- bundles to ranvier-

---
+++ commands/inventory.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = (srcPath) => {
+  const Broadcast = require(srcPath + 'Broadcast');
+
+  return {
+    usage: 'inventory',
+    command : (state) => (args, player) => {
+      if (!player.inventory || !player.inventory.size) {
+        return Broadcast.sayAt(player, "You aren't carrying anything.");
+      }
+
+      Broadcast.sayAt(player, "You are carrying:");
+
+      // TODO: Implement grouping
+      for (const [, item ] of player.inventory) {
+        Broadcast.sayAt(player, item.name);
+      }
+    }
+  };
+};
---
+++ commands/who.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = (srcPath) => {
+  const Broadcast = require(srcPath + 'Broadcast');
+
+  return {
+    usage: 'who',
+    command: (state) => (args, player) => {
+      Broadcast.sayAt(player, "<bold><red>                  Who's Online</bold></red>");
+      Broadcast.sayAt(player, "<bold><red>===============================================</bold></red>");
+      Broadcast.sayAt(player, '');
+
+      let numPlayers = 0;
+      state.PlayerManager.players.forEach((otherPlayer) => {
+        numPlayers++;
+        Broadcast.sayAt(player, ' * ' + otherPlayer.name);
+      });
+
+      Broadcast.sayAt(player, numPlayers + ' total');
+    }
+  };
+};
