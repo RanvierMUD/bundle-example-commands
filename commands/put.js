66dac7e909d7d77b9151f55fc696bb0df5640e94
Jared <neuralsplash@gmail.com>
2019-03-01 10:52:36 -0700

    update regex so it doesn't match substrings...such as 'linen' or 'phosphatidylcholine'

--- commands/put.js
+++ commands/put.js
@@ -15,7 +15,7 @@ module.exports = {
     }
 
     // put 3.foo in bar -> put 3.foo bar -> put 3.foo into bar
-    const parts = args.split(' ').filter(arg => !arg.match(/in/) && !arg.match(/into/));
+    const parts = args.split(' ').filter(arg => !arg.match(/^in$/) && !arg.match(/^into$/));
 
     if (parts.length === 1) {
       return B.sayAt(player, "Where do you want to put it?");
